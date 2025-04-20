2b82d0833e912ee9b2a788ea56723874376a91fd
fostlia <fostliacat@gmail.com>
2025-04-20 16:07:28 -0400

    Code cleanup, constants and comments.

--- backend/createavatar.py
+++ backend/createavatar.py
@@ -1,30 +1,34 @@
+# Using the Pillow library
+# > https://pillow.readthedocs.io/en/stable/handbook/tutorial.html
+##########
 import PIL
 import PIL.Image
 
+# Important constants for generating sprites
+AVATAR_RESIZE    = (56,56)          # Scale for the avatar in order to fit within the base sprite.
+SPRITE_DIMENSION = (128,128)        # Dimensions of the base sprite.
+AVATAR_PASTE_LOC = (36,30)          # X/Y coordinates for the top-left pixel of the resized avatar.
 
-
-
-
+# create_avatar()
+####################################
+# Creates an avatar sprite for user using the given file.
+# username - The name to write in the filename
+# filename - The file containing the user's original avatar
+####################################
 def create_avatar(username, filename):
-    myimage = PIL.Image.open("../frontend/public/sprites/player_base.png")
+    base_sprite = PIL.Image.open("../frontend/public/sprites/player_base.png")
 
-    # Avatar to resize 56x56
+    # Resize the Avatar to the correct dimensions to merge into the sprite.
     myavatar = PIL.Image.open(filename)
-    myavatar = myavatar.resize((56,56))
+    myavatar = myavatar.resize(AVATAR_RESIZE)
 
-    #print(myimage.format, myimage.size, myimage.mode)
+    # Create a new image, and combine both sprites into it.
+    new = PIL.Image.new("RGBA",SPRITE_DIMENSION)
+    new.paste(base_sprite)                      # Paste the base sprite first.
+    new.paste(myavatar, AVATAR_PASTE_LOC)       # Paste the avatar on top of the base, in the correct position.
 
-    new = PIL.Image.new("RGBA",(128,128))
-
-    #region = (36,30,91,85)
-
-    new.paste(myimage)
-    new.paste(myavatar, (36,30))
-
-    #new.show()
-
+    # Save the file.
     out_path = "../frontend/public/avatars/avsprite_" + username + ".png"
-
     new.save(out_path,"PNG")
     return
 
